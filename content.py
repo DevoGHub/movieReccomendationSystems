97a91cd88980e77e99c13a961d9cb12c4f4a9c5b
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2020-09-24 16:40:01 +0530

    Added gitignore

--- content.py
+++ content.py
@@ -22,9 +22,9 @@ movie_index = get_index_from_title(movie_user_likes)
 similar_movies = list(enumerate(cosine_sim[movie_index]))
 sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)[1:]
 i=0
-print("Top 5 similar movies to "+movie_user_likes+" are:\n")
+print("Top 15 similar movies to "+movie_user_likes+" are:\n")
 for element in sorted_similar_movies:
  print(get_title_from_index(element[0]))
  i=i+1
- if i>=5:
+ if i>=15:
      break
